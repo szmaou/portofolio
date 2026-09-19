3e390d07057eac7cbcfe135c126aec56df7ea3bb
szmaou <szmaou@gmail.com>
2026-09-19 21:42:03 +0700

    style(run): normalize indentation in run.sh

--- run.sh
+++ run.sh
@@ -2,18 +2,18 @@
 PORT=${2:-8080}
 
 case "$1" in
-  --docker)
-    docker compose up -d --build
-    echo "→ Portofolio running at http://localhost:$PORT"
-    ;;
-  --python)
-    python3 -m http.server "$PORT"
-    ;;
-  --npx)
-    npx live-server --port="$PORT" --no-browser
-    ;;
-  *)
-    echo "Usage: ./run.sh --docker | --python | --npx [port]"
-    exit 1
-    ;;
+    --docker)
+        docker compose up -d --build
+        echo "→ Portofolio running at http://localhost:$PORT"
+        ;;
+    --python)
+        python3 -m http.server "$PORT"
+        ;;
+    --npx)
+        npx live-server --port="$PORT"
+        ;;
+    *)
+        echo "Usage: ./run.sh --docker | --python | --npx [port]"
+        exit 1
+        ;;
 esac
